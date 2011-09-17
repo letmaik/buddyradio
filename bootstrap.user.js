b9fba43c0cbef574d0d2bb56d865684a3b79988d
Maik Riechert <maik.riechert@arcor.de>
2011-09-17 16:26:53 +0200

    added greasemonkey header

--- bootstrap.user.js
+++ bootstrap.user.js
@@ -1,3 +1,12 @@
+// BuddyRadio v0.1
+//
+// ==UserScript==
+// @name          BuddyRadio
+// @namespace     http://github.com/neothemachine/
+// @description   tbd
+// @include       http://grooveshark.com/*
+// ==/UserScript==
+
 var s = document.createElement("script");
 s.type = "text/javascript";
 s.src = "http://code.onilabs.com/apollo/0.12/oni-apollo.js";
