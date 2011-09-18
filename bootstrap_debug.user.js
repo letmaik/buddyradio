5d0d373076e54b20100aa689921a8acf183e5cc5
Maik Riechert <maik.riechert@arcor.de>
2011-09-18 23:54:52 +0200

    created model in coffeescript

--- bootstrap_debug.user.js
+++ bootstrap_debug.user.js
@@ -16,35 +16,96 @@ s.type = "text/javascript";
 //s.src = "http://code.onilabs.com/apollo/0.12/oni-apollo.js";
 // need to use trunk version to have callback on require()
 s.src = "http://code.onilabs.com/apollo/unstable/oni-apollo.js";
-s.addEventListener("load", loadBuddyRadioModuleOnDelay, false);
+s.addEventListener("load", loadCoffeeOnDelay, false);
 document.body.appendChild(s);
 
-function loadBuddyRadioModuleOnDelay() {
-	setTimeout(loadBuddyRadioModule, 666);
+function loadCoffeeOnDelay() {
+	setTimeout(loadCoffee, 666);
 }
 
-function loadBuddyRadio(err, module) {
+function loadCoffee() {
+	unsafeWindow.require("github:onilabs/coffee-script/master/extras/coffee-script.js", {callback: coffeeLoaded});
+}
+
+function coffeeLoaded(err, module) {
+	if (err) throw ('error: ' + err);
+
+	var debugMultiLineHack = (<><![CDATA[
+
+# START
+	
+exports.start = () ->
+	alert("a")
+
+Model = {}
+	
+class Model.SongRessource
+	play: -> throw new Error("must be overriden")
+	
+class Model.GroovesharkSongRessource extends Model.SongRessource
+	constructor(@songId) ->
+	play: -> # todo
+		
+class Model.Song
+	constructor(@artist, @title, @listenedAt) -> # unix timestamp
+	length: null    # song length in seconds
+	ressources: []	
+	
+class Model.Buddy
+	constructor: (@username, @listeningStatus) ->
+	
+	currentSong: null
+	pastSongs: []    
+	
+	refreshListeningData: -> throw new Error("must be overriden")
+
+class Model.LastFmBuddy extends Model.Buddy
+	refreshListeningData: ->
+		listeningStatus = "live"
+		#currentSong = 
+		#pastSongs..
+	
+	
+class Model.BuddyRessource
+	name: "Buddy Ressource Name" # used in links etc.
+	loadBuddy: (buddyId) -> throw new Error("must be overriden")
+	
+class Model.LastFmBuddyRessource extends Model.BuddyRessource
+	name: "Last.fm"
+	loadBuddy: (username) ->
+		# do stuff & return LastFmBuddy object
+		
+class Model.BuddyManager
+	buddies: []
+	refreshListeningData: () ->
+		buddy.refreshListeningData() for buddy in buddies
+		null
+	
+class Model.Radio
+	constructor: (@buddyRessources) ->
+	
+	buddyManager: new Model.BuddyManager
+	onairBuddy: null  
+	playingSong: null 
+	playingPosition: null # playing position in seconds
+	queuedSongs: []   
+	playedSongs: []  
+	isAlternativeSong: false
+	noSongFound: false
+	
+	
+# END
+	
+	]]></>).toString();
+	
+	unsafeWindow.CScript = module.CoffeeScript;
+	unsafeWindow.CS = debugMultiLineHack;
+	var sjsSrc = module.CoffeeScript.compile(debugMultiLineHack);
+	unsafeWindow.require("local:buddyradio", {callback: radioLoaded, src: sjsSrc});
+}
+
+function radioLoaded(err, module) {
 	if (err) throw ('error: ' + err);
 	module.start();
 }
-
-function loadBuddyRadioModule() {
-
-	var debugMultiLineHack = (<><![CDATA[
-// START
-
-exports.start = function() {
-	window.console.log(document);
-	window.console.log(window);
-}
-
-
-
-// END
-	]]></>).toString();
-
-	unsafeWindow.require("local:buddyradio", {callback: loadBuddyRadio, src: debugMultiLineHack});
-}
-
-
 
